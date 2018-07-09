3a6dc7f577660809240178be9051518ee4a1bac3
Pamela Bembe Ianovalli <pamela.novalli@gmail.com>
2018-07-08 21:42:38 -0300

    Project setup (#1)
    * router adicionado
    
    * router adicionado

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Footer;
