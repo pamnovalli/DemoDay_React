98e12c4f29c40555a5661b5c3496beaf0afceb9c
Felipe Antonio Cardoso <felipe.cardoso@nextel.com.br>
2018-07-09 17:00:48 -0300

    Added content section

--- src/screen/app/App.js
+++ src/screen/app/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainSection from '../../components/mainSection'
+import ScrollableAnchor from 'react-scrollable-anchor'
+import ContentSection from '../../components/contentSection';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainSection />
+        <ScrollableAnchor id={'search'}>
+          <MainSection />
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'project'}>
+          <ContentSection />
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'about'}>
+          <ContentSection />
+        </ScrollableAnchor>
       </div>
     );
   }
