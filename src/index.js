32c9a40f21ec18448307651ea9a3863c045b3a1f
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-08 21:19:31 -0300

    router adicionado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './screen/app/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <Switch>
+        <Route path="/" exact={true} component={App} />
+    </Switch>
+</ BrowserRouter>,  document.getElementById('root'));
 registerServiceWorker();
