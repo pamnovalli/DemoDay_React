721d8e15da6640edfa988650e703a35f5ff1c751
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-09 19:35:17 -0300

    Added responsive footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  } from 'reactstrap';
+
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md" fixed={`bottom`}>
+          <NavbarBrand href="/">Contatos</NavbarBrand>
+            <Nav className="mx-auto">
+              <NavItem>
+                <NavLink href="/">Gmail</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Facebook</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
--- src/screen/app/App.js
+++ src/screen/app/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MainSection from '../../components/mainSection';
 import ContentSection from '../../components/contentSection';
 import Menu from '../../components/menu';
+import Footer from '../../components/footer';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <MainSection />
         <ContentSection id={'project'} title="Teste1" content="bla bla 2"/>
         <ContentSection id={'about'} title="Teste2" content="bla bla 2" />
+        <Footer />
       </div>
     );
   }
