e559b6be59a05f19021ce2acc07df75f52ceb660
Ana Carvalho <ana.s.carvalho@outlook.com.br>
2018-07-10 18:11:46 -0300

    Acrescentar tela de cadastro

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screen/app/App';
+import Cadastro from './screen/cadastro';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ ReactDOM.render(
 <BrowserRouter>
     <Switch>
         <Route path="/" exact={true} component={App} />
+        <Route path="/cadastro" exact={true} component={Cadastro} />
     </Switch>
 </ BrowserRouter>,  document.getElementById('root'));
 registerServiceWorker();
