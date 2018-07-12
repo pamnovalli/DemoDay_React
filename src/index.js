6ddfc0e98d09eb8bb885576562c322758f18828a
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-12 18:05:50 -0300

    Adicionar tela ong

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screen/app/App';
 import Login from './screen/login';
+import Ong from './screen/ong';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ ReactDOM.render(
 <BrowserRouter>
     <Switch>
     <Route path="/login" exact={true} component={Login} />
+    <Route path="/ong" exact={true} component={Ong} />
     <Route path="/" exact={true} component={App} />
     </Switch>
 </ BrowserRouter>,  document.getElementById('root'));
