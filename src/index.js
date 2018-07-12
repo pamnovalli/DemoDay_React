d1190c44108a87d6ac418816f81f653367b7d810
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-11 21:36:36 -0300

    Acrescentar tela de vitrine

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screen/app/App';
+import Vitrine from './screen/vitrine';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ ReactDOM.render(
 <BrowserRouter>
     <Switch>
         <Route path="/" exact={true} component={App} />
+        <Route path="/vitrine" exact={true} component={Vitrine} />
     </Switch>
 </ BrowserRouter>,  document.getElementById('root'));
 registerServiceWorker();
