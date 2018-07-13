697a462644390a3b578425d3a199daa40e0f0fca
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-13 13:26:33 -0300

    Atualizar tela de Perfil

--- src/screen/perfil/index.js
+++ src/screen/perfil/index.js
@@ -17,35 +17,39 @@ export default class Perfil extends React.Component {
                         <h3>Minhas Ongs</h3>
                         <NavLink href="">Lingua estrangeira</NavLink>
                         <NavLink href="">Esporte</NavLink>
-                        <Button className="VerTodos">+ Ver todos</Button>
+                        <NavLink id="vertodos">+ Ver Todos</NavLink>
                     </Nav> 
                     <Nav id="grupos">
                         <h3>Meus Grupos</h3>
                         <NavLink href="">Lingua estrangeira</NavLink>
                         <NavLink href="">Esporte</NavLink>
-                        <Button className="VerTodos">+ Ver todos</Button>
+                        <NavLink id="vertodos">+ Ver Todos</NavLink>
+                        <NavLink id="criargrupos">Criar Grupos</NavLink>
                     </Nav> 
                 </Container>
+                <Container className="SessaoDois">
                     <Card className= "Card">
                         <CardBody>
                             <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
                             <CardTitle>Perfil</CardTitle>
                         </CardBody>
                     </Card>
-            </Container>
-            <Button id="criargrupo">Criar Grupo</Button>
-                <Container className="Interesses">
+                    <Container className="Interesses">
                     <ButtonGroup className="Tags">
                         <Button outline color="primary">Tecnologia</Button>{' '}
                         <Button outline color="secondary">Ambientalismo</Button>{' '}
-                        <Button outline color="success">Esporte</Button>{' '}
                     </ButtonGroup>
                     <ButtonGroup className="Tags">
                         <Button outline color="info">Culinaria</Button>{' '}
                         <Button outline color="warning">Proteção Animal</Button>{' '}
-                        <Button outline color="danger">Lingua estrangeira</Button>
+                    </ButtonGroup>
+                    <ButtonGroup className="Tags">
+                        <Button outline color="success">Lingua estrangeira</Button>{' '}
+                        <Button outline color="success">Esporte</Button>{' '}
                     </ButtonGroup>
                 </Container>
+                </Container>
+            </Container>
             <Footer />
         </div>
         );
