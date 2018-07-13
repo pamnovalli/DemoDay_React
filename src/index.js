8739139d5ee27b34bfeb040113b9cb88c07af7d7
Ana Carvalho <ana.s.carvalho@outlook.com.br>
2018-07-13 14:47:37 -0300

    Adicionar tela de criar grupos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screen/app/App';
+import CriarGrupo from './screen/criarGrupo';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ ReactDOM.render(
 <BrowserRouter>
     <Switch>
         <Route path="/" exact={true} component={App} />
+        <Route path="/criarGrupo" exact={true} component={CriarGrupo} />
     </Switch>
 </ BrowserRouter>,  document.getElementById('root'));
 registerServiceWorker();
