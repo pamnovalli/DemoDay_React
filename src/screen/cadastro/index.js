20cbd6750b55213c41845a801362fe8b0b72258f
Felipe Antonio Cardoso <felipe.cardoso@nextel.com.br>
2018-07-14 17:38:38 -0300

    fix signup screen

--- src/screen/cadastro/index.js
+++ src/screen/cadastro/index.js
@@ -1,103 +1,134 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 import Menu from '../../components/menu';
 import Footer from '../../components/footer';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 
-export default class Cadastro extends React.Component {
+class SingUpForm extends Component {
     render() {
         return (
-            
             <div>
-            <Menu />
-            <br/>
-            <br/>
-            <h1>Cadastre-se</h1>
-            <Form>
-            <FormGroup className="Input">
-            <p>Nome</p>
-            <Input type="text" name="nome" id="Nome" placeholder="Insira nome e sobrenome:" />
-            </FormGroup>
-            <FormGroup className="Input">
-            <p>E-mail</p>
-            <Input type="email" name="email" id="Email" placeholder="Insira seu e-mail:" />
-            </FormGroup>
-            <FormGroup className="Input">
-            <p>RG</p>
-            <Input type="text" name="rg" id="RG" placeholder="Insira somente números:" />
-            </FormGroup>
-            <div id="senha"> 
-            <FormGroup className="Input">
-            <p>Senha</p>
-            <Input type="password" name="senha" id="Senha" placeholder="Crie sua senha:" />
-            <p id="obs">*use de 6 a 20 caracteres</p>
-            </FormGroup>
-            <FormGroup className="Input">
-            <Input type="password" name="csenha" id="Csenha" placeholder="Confirme sua senha:" />
-            </FormGroup>
+                <Form className="SingUpFormContainer">
+                    <h1>Cadastre-se</h1>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="name">Nome</Label>
+                        <Input type="text" name="nome" id="Nome" placeholder="Insira nome e sobrenome:" />
+                    </FormGroup>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="email">Email</Label>
+                        <Input type="email" name="email" id="Email" placeholder="Insira seu e-mail:" />
+                    </FormGroup>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="password">Password</Label>
+                        <Input type="password" name="senha" id="Senha" placeholder="Crie sua senha:" />
+                        <FormText color="muted">*use de 6 a 20 caracteres</FormText>
+                    </FormGroup>
+                        <FormGroup className="SignUpFormGroup">
+                        <Label for="password-confirmation">Confirmação de senha</Label>
+                        <Input type="password" name="csenha" id="Csenha" placeholder="Confirme sua senha:" />
+                    </FormGroup>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="gender">Genero</Label>
+                        <Input type="select" name="select" id="gender-field">
+                            <option>Feminino</option>
+                            <option>Masculino</option>
+                            <option>Outro</option>
+                            <option>Prefiro não informar</option>
+                        </Input>
+                    </FormGroup>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="profile-pic">Foto do perfil</Label>
+                        <Input type="file" name="file" id="exampleFile" />
+                        <FormText color="muted"> Escolha uma foto com resolução de 300X300 pixels </FormText>
+                    </FormGroup>
+                    
+                    <p className="SignUpFormGroup">Selecione seus interesses:</p>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Língua Estrangeira
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Tecnologia
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Ambientalismo
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Esporte
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Culinária
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Culinária
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Culinária
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Proteção Animal
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Assistência à moradores de rua
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Alfabetização de adultos
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Crianças carentes
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Idosos
+                        </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" /> Outros
+                        </Label>
+                    </FormGroup>
+                    <FormGroup className="SignUpFormGroup">
+                        <Label for="profile-pic">Nos conte sobre seus objetivos ao se cadastrar em nossa plataforma:</Label>
+                        <Input type="textarea" name="text" id="exampleText" />
+                    </FormGroup>
+                    <Button>Enviar</Button>
+                </Form>
             </div>
-            <FormGroup className="Input">
-            <p>Sexo</p>
-            <Input type="select" name="select" id="exampleSelect">
-            <option>Feminino</option>
-            <option>Masculino</option>
-            <option>Outro</option>
-            <option>Prefiro não informar</option>
-            </Input>
-            </FormGroup>
-            <FormGroup className="Input">
-            <p>Foto de Perfil</p>
-            <FormText color="muted">
-            Escolha uma foto com resolução de 300X300 pixels
-            </FormText>
-            <Input type="file" name="file" id="exampleFile" />
-            </FormGroup>
-            <br/>
-            <p className="Input">Selecione seus interesses:</p>
-            <FormGroup className="Input" check>
-            <Input type="checkbox" />{' '}
-            Língua Estrangeira
-            <br/>
-            <Input type="checkbox" />{' '}
-            Tecnologia
-            <br/>
-            <Input type="checkbox" />{' '}
-            Ambientalismo
-            <br/>
-            <Input type="checkbox" />{' '}
-            Esporte
-            <br/>
-            <Input type="checkbox" />{' '}
-            Culinária
-            <br/>
-            <Input type="checkbox" />{' '}
-            Proteção Animal
-            <br/>
-            <Input type="checkbox" />{' '}
-            Assistência à moradores de rua
-            <br/>
-            <Input type="checkbox" />{' '}
-            Alfabetização de adultos
-            <br/>
-            <Input type="checkbox" />{' '}
-            Crianças carentes
-            <br/>
-            <Input type="checkbox" />{' '}
-            Idosos
-            <br/>
-            <Input type="checkbox" />{' '}
-            <Input type="text" name="nome" id="Outro" placeholder="Outros" />
-            <br/>
-            <br/>
-            </FormGroup>
-            <FormGroup className="Input">
-            <p>Nos conte sobre seus objetivos ao se cadastrar em nossa plataforma:</p>
-            <Input type="textarea" name="text" id="exampleText" />
-            </FormGroup>
-            <Button>Enviar</Button>
-            </Form>  
-            <Footer />
+        )
+    }
+}
+
+export default class Cadastro extends Component {
+    render() {
+        return (
+            <div>
+                <Menu />
+                <div className="SingUpContainer">
+                    <SingUpForm />  
+                </div>
+                <Footer />
             </div>
         );
     }
