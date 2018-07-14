910e5cf8731d2fc1a016038a29b89b04d5f4c729
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-13 23:40:43 -0300

    Acrescentar responsividade

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -23,15 +23,15 @@ export default class Footer extends React.Component {
           <NavbarBrand href="/">Contatos</NavbarBrand>
             <Nav className="mx-auto">
               <NavItem className="Sociais">
-              <img height="42" width="42" src='https://cdn.icon-icons.com/icons2/535/PNG/128/Email-Icon_icon-icons.com_52870.png' />
+              <img height="35" width="35" src='https://cdn.icon-icons.com/icons2/535/PNG/128/Email-Icon_icon-icons.com_52870.png' />
                 <NavLink href="/">Gmail</NavLink>
               </NavItem>
               <NavItem className="Sociais">
-              <img height="42" width="42" src='https://cdn.icon-icons.com/icons2/555/PNG/128/facebook_icon-icons.com_53612.png' />
+              <img height="35" width="35" src='https://cdn.icon-icons.com/icons2/555/PNG/128/facebook_icon-icons.com_53612.png' />
                 <NavLink href="/">Facebook</NavLink>
               </NavItem>
               <NavItem className="Sociais">
-              <img height="42" width="42" src='https://cdn.icon-icons.com/icons2/836/PNG/128/Instagram_icon-icons.com_66804.png' />
+              <img height="35" width="35" src='https://cdn.icon-icons.com/icons2/836/PNG/128/Instagram_icon-icons.com_66804.png' />
                 <NavLink href="/">Instagram</NavLink>
               </NavItem>
             </Nav>
--- src/screen/login/index.js
+++ src/screen/login/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
 import Menu from '../../components/menu';
 import Footer from '../../components/footer';
 import { InputGroup, Form, Button, Input } from 'reactstrap';
 
-const Login = (props) => {
-  return (
+class Login extends Component {
+  render() {
+    return (
     <div>
-      <Menu />
-      <Form className="form">
+    <Menu />
+      <Form className="Form">
         <h1> Voluntarize </h1>
-        <InputGroup className="input">
+        <InputGroup className="Input">
           <Input placeholder="username" />
         </InputGroup>
         <br />
-        <InputGroup className="input">
+        <InputGroup className="Input">
           <Input placeholder="yourpassword" />
         </InputGroup>
-        <Button color="primary">Login</Button>
+        <Button id="login" color="primary">Login</Button>
       </Form>
       <Footer />
     </div>
   );
-};
+}
+}
 
 export default Login;
 
