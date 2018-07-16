71792a00b6d8f1cc86f4a2e31b8e877c0cb56141
Pamela Ianovalli <pamela.novalli@gmail.com>
2018-07-15 22:36:43 -0300

    Atualizações

--- src/screen/perfil/index.js
+++ src/screen/perfil/index.js
@@ -14,27 +14,48 @@ import {
     CardBody,
     CardTitle
 } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 
 export default class Perfil extends Component {
 
+   
+    communityPanel = () => (
+        <div className="CommunityPanel">
+            <div className="Community">
+                <h2>Minhas ONGs</h2>
+                <Link to={"/ong"}>Teto</Link>
+                <Link to={"/ong"}>Teto</Link>
+                <Link to={"/ong"}>Teto</Link>
+            </div>
+            <div className="Community">
+                <h2>Meus Grupos</h2>
+                <Link to={"/ong"}>Teto</Link>
+                <Link to={"/ong"}>Teto</Link>
+                <Link to={"/ong"}>Teto</Link>
+            </div>
+        </div>
+    )
+
     userProfilePanel = () => (
         <div className="UserProfilePanel">
-            <img  id="fotoperfil" width="150px" heigth="150px" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-            <h1 id="nome">Nome do Usuário</h1>
+            <img  id="fotoperfil" width="60px" heigth="50px" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
             <Tags tags = {["culinaria","tecnologia","lingua estrangeira","artesanato"]}/>
         </div>
     )
+
 
     render() {
         return (
             <div>
                 <Menu />
                 <div className="UserProfileContainer">
+                    {this.communityPanel()}
                     {this.userProfilePanel()}
-                </div>
+                </div>>
                 <Footer />
             </div>
         );
     }
 }
+
