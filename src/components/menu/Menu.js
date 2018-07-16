60d5cd588799bac6a8e8112ff19c537f285f4e38
Felipe Antonio Cardoso <felipe.antonio.cardoso@gmail.com>
2018-07-16 00:30:35 -0300

    Revert "Link para as paginas de Perfil e Detalhe de Organização (#14)"
    This reverts commit d5abed048218d4d3fbb2f9e3024421e369eaa2c7.

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Menu.css';
+
+
+class Menu extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './screen/app/App';
-import Vitrine from './screen/vitrine';
-import Login from './screen/login';
-import Cadastro from './screen/cadastro';
-import Perfil from './screen/perfil';
-import Ong from './screen/ong';
-import CriarGrupo from './screen/criarGrupo';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
 <BrowserRouter>
     <Switch>
-        <Route path="/vitrine"  exact={false} component={Vitrine} />
         <Route path="/" exact={true} component={App} />
-        <Route path="/cadastro" exact={true} component={Cadastro} />
-        <Route path="/login" exact={true} component={Login} />
-        <Route path="/perfil" exact={true} component={Perfil} />
-        <Route path="/criarGrupo" exact={true} component={CriarGrupo} />
-        <Route path="/ong" exact={true} component={Ong} />
     </Switch>
 </ BrowserRouter>,  document.getElementById('root'));
 registerServiceWorker();
